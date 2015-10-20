8fedc33ba862bbfbcf8957c5199b113a839888e5
Vitor Pereira dos Santos <vitor_gja_@hotmail.com>
2015-10-20 18:52:54 +0000

    UPDATE js/layout.css

--- js/layout.js
+++ js/layout.js
@@ -1,85 +1,43 @@
-$(main);
-function main(){
-	$(".hidden").hide();
-	$(".alert-success").hide();
-	$(".alert-danger").hide();
-	$("#enviar").click(function(){
-		var nome = $("#nome").val();
-		var email = $("#email").val();
-		var assunto = $("#assunto").val();
-		var msg = $("#msg").val();
+function submit(){
+	// BOTÂO DE ENVIAR
+	$("button[data-input=submit], input[data-input=submit]").click(function(){
+		var nome = $("input[name='nome']").val();
+		var email = $("input[name='email']").val();
+		var assunto = $("input[name='assunto'], select[name='assunto']").val();
+		var mensagem = $("textarea[name='mensagem']").val();
 
-		var limpar = function(){
-			$("#nome").val("");
-			$("#email").val("");
-			$("#assunto").val("");
-			$("#msg").val("");
-		};
-		var limparHide = function(){
-			$(".alert-success").hide();
-			$(".alert-danger").hide();
-		}
-		// Validação
-		$("#nome").focus(function(){	$("label[for=nome]").html("Nome:");	});
-		$("#email").focus(function(){	$("label[for=email]").html("Email:");	});
-		$("#assunto").focus(function(){	$("label[for=assunto]").html("Assunto:");	});
-		$("#msg").focus(function(){		$("label[for=msg]").html("Mensagem:");	});
-		if(nome==""){
-			$("label[for=nome]").html('Nome:   <span class="text-red"><small>* Preencha o Campo Nome</small></span>');
-		}
-		if(email==""){
-			$("label[for=email]").html('Email:   <span class="text-red"><small>* Preencha o Campo Email</small></span>');
-		}
-		if(assunto==""){
-			$("label[for=assunto]").html('Assunto:   <span class="text-red"><small>* Preencha o Campo Assunto</small></span>');
-		}
-		if(msg==""){
-			$("label[for=msg]").html('Mensagem:   <span class="text-red"><small>* Preencha o Campo Mensagem</small></span>');
-		}
-		
-
-		if((nome!="")&&(email!="")&&(assunto!="")&&(msg!="")){
-			$.post("http://sifatec.esy.es/vitorgja.github.io/enviar.php", {
-				nome: nome, 
-				email: email, 
-				assunto: assunto, 
-				mensagem: msg }, function(data){
-
-				if(data=="true"){
-					$(".alert-success").html("Mensagem Enviada com sucesso!");
-					$(".alert-danger").hide();
-					$(".alert-success").show();
-					setTimeout(function(){ 
-						$(".alert-success").hide();
-						$(".alert-danger").hide(); 
-					}, 3000);
-					limpar();
-				}else if(data=="erro-1"){
-					$(".alert-danger").html("Erro  no envio da Mensagem!");
-					$(".alert-success").hide();
-					$(".alert-danger").show();
-					setTimeout(function(){ 
-						$(".alert-success").hide();
-						$(".alert-danger").hide(); 
-					}, 3000);
-				}else if(data=="erro-2"){
-					$(".alert-danger").html("Preencha todos os campos para enviar a Mensagem!");
-					$(".alert-success").hide();
-					$(".alert-danger").show();
-					setTimeout(function(){ 
-						$(".alert-success").hide();
-						$(".alert-danger").hide(); 
-					}, 3000);
+		$.ajax({
+			type: "POST",
+			url: "enviar.php",
+			dataType : "json",
+			data: {
+				nome: nome,
+				email: email,
+				assunto: assunto,
+				mensagem: mensagem
+			},
+			success: function(data){
+				if(data.tipo == "sucesso"){	// Enviou
+					BootstrapDialog.show({
+						title: 'Sucesso',
+						type: BootstrapDialog.TYPE_SUCCESS, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
+						message: data.mensagem
+					});
+				} else if(data.tipo == "erro-01"){ // Erro ao enviar
+					BootstrapDialog.show({
+						title: 'Erro',
+						type: BootstrapDialog.TYPE_DANGER, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
+						message: data.mensagem
+					});
+				} else if(data.tipo == "erro-02"){ // Erro nao preencheu todos os campos
+					BootstrapDialog.show({
+						title: 'Erro',
+						type: BootstrapDialog.TYPE_DANGER, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
+						message: data.mensagem + '\n' + data.campos
+					});
 				}
-			});
-		}else{
-			$(".alert-danger").html("Preencha todos os campos para enviar a Mensagem!");
-			$(".alert-success").hide();
-			$(".alert-danger").show();
-			setTimeout(function(){ 
-				$(".alert-success").hide();
-				$(".alert-danger").hide(); 
-			}, 3000);
-		}
+			}
+
+		});
 	});
 }
